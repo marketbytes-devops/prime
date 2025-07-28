64721f35f9f174ccdce776f8192a1adb099015b5
ajay-marketbytes <ajay@marketbytes.in>
2025-07-28 11:24:26 +0530

    Updated the button disable func

--- dashboard/src/components/Button/index.jsx
+++ dashboard/src/components/Button/index.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ onClick, children, className }) => {
+const Button = ({ onClick, children, className, disabled }) => {
   return (
     <button
-      onClick={onClick}
-      className={`w-full p-2 rounded transition-opacity duration-300 opacity-90 ${className}`}
+      onClick={disabled ? undefined : onClick} 
+      className={`w-full p-2 rounded transition-opacity duration-300 opacity-90 ${
+        disabled ? 'opacity-50 cursor-not-allowed' : 'hover:opacity-100'
+      } ${className}`}
+      disabled={disabled} 
     >
       {children}
     </button>
@@ -15,11 +18,13 @@ Button.propTypes = {
   onClick: PropTypes.func,
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  disabled: PropTypes.bool,
 };
 
 Button.defaultProps = {
-  onClick: () => { },
+  onClick: () => {},
   className: '',
+  disabled: false,
 };
 
 export default Button;
