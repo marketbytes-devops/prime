1c440f498560a0874889c101381c51733ea6eff6
ajay-marketbytes <ajay@marketbytes.in>
2025-09-01 15:05:33 +0530

    updates::Authapp base

--- backend/authapp/models.py
+++ backend/authapp/models.py
@@ -2,7 +2,33 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email field must be set'))
+        email = self.normalize_email(email)
+        extra_fields.setdefault("username", email.split("@")[0])  
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("username", email.split("@")[0]) 
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
     name = models.CharField(max_length=255, blank=True)
@@ -17,6 +43,8 @@ class CustomUser(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+    
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
